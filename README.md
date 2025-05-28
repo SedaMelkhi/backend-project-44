fccd4c52d3d499e40426c7b33958ee3d204a7fbf
SedaMelkhi <31256499+SedaMelhi@users.noreply.github.com>
2025-05-28 17:59:06 +0300

    реализовала игру для поиска НОД

--- README.md
+++ README.md
@@ -4,8 +4,11 @@
 
 [![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=SedaMelkhi_backend-project-44&metric=alert_status)](https://sonarcloud.io/project/overview?id=SedaMelkhi_backend-project-44)
 
-# Запись игры brain-even
+### Запись игры brain-even
 https://asciinema.org/a/5b678G0fBeOYsfGG9WwYkfH6j
 
-# Запись игры brain-calc
+### Запись игры brain-calc
 https://asciinema.org/a/Cn2yU108TV1Cka9y7yh9TcL34
+
+### Запись игры brain-gcd
+https://asciinema.org/a/1WpZZS6il1dftsALVT2UWAqWB
--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -5,14 +5,18 @@ import { getName } from '../src/cli.js'
 const brainCalc = (name, count = 0) => {
   const firstNum = Math.round(Math.random() * 100)
   const secondNum = Math.round(Math.random() * 100)
-  const actionNum = Math.round(Math.random() * 10) % 3 === 0
-  const actionSymbol = actionNum % 3 === 0 ? '+' : actionNum % 3 === 1 ? '-' : '*'
   if (count === 0) {
-    console.log('What is the result of the expression?')
+    console.log('Find the greatest common divisor of given numbers.')
+  }
+  let correctAnswer = 1
+  for (let i = 0; i <= firstNum; i++) {
+    if (firstNum % i === 0 && secondNum % i === 0) {
+      correctAnswer = String(i)
+    }
   }
   playRound({
-    question: `${firstNum} ${actionSymbol} ${secondNum}`,
-    correctAnswer: String(actionSymbol === '*' ? firstNum * secondNum : actionSymbol === '+' ? firstNum + secondNum : firstNum - secondNum),
+    question: `${firstNum} ${secondNum}`,
+    correctAnswer,
     count,
     name,
     func: brainCalc,
