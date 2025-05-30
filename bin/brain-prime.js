73424623299dbddf0c269faabb71b96aaa509a01
SedaMelkhi <31256499+SedaMelhi@users.noreply.github.com>
2025-05-30 16:58:34 +0300

    единица теперь не считается простым числом

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -5,7 +5,7 @@ const brainPrime = () => {
   const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
   const generateQuestionAndAnswer = () => {
     const randomNumber = Math.round(Math.random() * 100)
-    let correctAnswer = 'yes'
+    let correctAnswer = randomNumber > 1 ? 'yes' : 'no'
     for (let i = 2; i < randomNumber; i++) {
       if (randomNumber % i === 0) {
         correctAnswer = 'no'
